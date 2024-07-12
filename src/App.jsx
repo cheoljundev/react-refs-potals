5eb511ac1d650b4407c53396391e831fb8b4fb5f
cheoljundev <cheoljundev@gmail.com>
2024-07-12 14:09:39 +0900

    타이머 설정 & State(상태) 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import Player from './components/Player.jsx';
 import TimerChallenge from "./components/TimerChallenge.jsx";
+import {useState} from "react";
 
 function App() {
-  return (
-    <>
-      <Player />
-      <div id="challenges">
-          <TimerChallenge title="Easy" targetTime={1} />
-          <TimerChallenge title="Not Easy" targetTime={5} />
-          <TimerChallenge title="Gatting tough" targetTime={10} />
-          <TimerChallenge title="Pros only" targetTime={15} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Player/>
+            <div id="challenges">
+                <TimerChallenge title="Easy" targetTime={1}/>
+                <TimerChallenge title="Not Easy" targetTime={5}/>
+                <TimerChallenge title="Gatting tough" targetTime={10}/>
+                <TimerChallenge title="Pros only" targetTime={15}/>
+            </div>
+        </>
+    );
 }
 
 export default App;
